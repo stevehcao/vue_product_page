238be73bb8adbbc0015d802c376fcd29a8640109
Steve Cao <steve.cao7@gmail.com>
2020-02-11 11:09:57 -0800

    toggle a basic vue component

---
+++ src/components/Carousel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="carousel-container">
+    I'm a carousel
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Carousel'
+}
+</script>
+
+<style scoped>
+  .carousel-container {
+    background-color: lightblue;
+  }
+</style>
